bf764bd96fde7da23fbdbced9dfed9fa702ccdea
jason aghedo <jason.aghedo@gmail.com>
2024-07-31 12:49:40 +0100

    feat: Responsive

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import EmailForm from "./components/EmailForm";
 
 function App() {
 	return (
-		<main className="w-screen h-screen p-5 overflow-hidden rounded-lg bg-[#F7F7F8]">
-			<section className="grid grid-cols-[auto_45vw] h-full rounded-lg overflow-hidden border border-[#040c2f15]">
-				<div className="w-full bg-white h-full py-20 px-10 flex flex-col justify-center">
-					<header className="mb-20">
-						<h1 className="font-extrabold tracking-normal font-dmSans text-[#020617] text-[40px] leading-[1.1em]">
+		<main className="w-screen h-[100dvh] lg:p-5 overflow-hidden bg-[#F7F7F8]">
+			<section className="grid md:grid-cols-[auto_45vw] h-full lg:rounded-lg md:overflow-hidden lg:border border-[#040c2f15]">
+				<div className="w-full bg-white h-full px-5 py-14 lg:py-20 lg:px-10 flex flex-col justify-start">
+					<header className="flex gap-x-3 items-center mb-10">
+						<img
+							className="lg:size-16 md:size-14 size-12"
+							src="/assets/logo.svg"
+							alt="logo icon"
+						/>
+						<h1 className="font-extrabold tracking-normal font-dmSans text-[#020617] text-[28px] sm:text-3xl lg:text-[40px] leading-[1.1em]">
 							Christian Writes
 						</h1>
 					</header>
-					<div className="font-inter flex flex-col gap-y-4 max-w-[500px]">
+					<div className="font-inter flex flex-col gap-y-6 max-w-[400px] lg:max-w-[500px] mt-auto">
 						<Banner />
-						<h2 className="font-semibold font-dmSans tracking-normal text-4xl leading-[1.1em]">
-							Ready to launch your <br /> product, course or app?
+						<h2 className="font-semibold font-dmSans tracking-normal text-3xl lg:text-4xl leading-[1.1em]">
+							Ready to launch your <br className="hidden sm:block" /> product,
+							course or app?
 						</h2>
 						<p className="text-[#334155] text-sm leading-[1.65em]">
 							Publish your own waitlist page in minutes to start capturing email
@@ -26,7 +32,7 @@ function App() {
 						<Counter />
 					</div>
 				</div>
-				<div className="w-full">
+				<div className="w-full hidden md:block">
 					<img
 						className="object-cover w-full h-full"
 						src="/assets/background.jpg"
--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -2,13 +2,14 @@ const EmailForm = () => {
 	return (
 		<form className="flex gap-x-3 ">
 			<input
-				type="text"
+				required
+				type="email"
 				className="p-2 w-full text-[#334155] border-b-[1.2px] border-[#020617] placeholder:text-[#33415599] max-w-[240px] focus:outline-none"
 				placeholder="name@domain.com"
 			/>
 			<button
 				type="submit"
-				className="bg-[#020617] px-6 py-3 text-sm rounded-md text-white text-nowrap font-semibold"
+				className="bg-[#020617] px-4 py-1 lg:px-6 lg:py-3 text-sm rounded-md text-white text-nowrap font-semibold"
 			>
 				Join Now
 			</button>
